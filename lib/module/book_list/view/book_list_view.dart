e2f05c501fa43d3293aa5ad0971094311baf3443
codekaze <codekaze.id@gmail.com>
2021-08-22 12:03:33 +0700

    Sun, Aug 22, 12:03:32

--- lib/module/book_list/view/book_list_view.dart
+++ lib/module/book_list/view/book_list_view.dart
@@ -1,3 +1,5 @@
+import 'package:codekaze_free_ui_kit/module/book_detail/view/book_detail_view.dart';
+import 'package:codekaze_free_ui_kit/module/clean_ecommerce_ui_kit/shared/widget/button/button.dart';
 import 'package:flutter/material.dart';
 import 'package:codekaze_free_ui_kit/module/book_list/controller/book_list_controller.dart';
 
@@ -15,6 +17,14 @@ class BookListView extends StatelessWidget {
         return Scaffold(
           appBar: AppBar(
             title: Text("Book List"),
+          ),
+          body: Column(
+            children: [
+              CeButton(
+                label: "Open Detail",
+                onPressed: () => Get.to(BookDetailView()),
+              ),
+            ],
           ),
         );
       },
