02d5792e0dddbb211294ba26fb158c9bc4c8e518
febriannisarisa <febriannisarisa21@gmail.com>
2021-08-23 10:11:46 +0700

    calendar

--- lib/module/course_calendar/view/course_calendar_view.dart
+++ lib/module/course_calendar/view/course_calendar_view.dart
@@ -1,5 +1,8 @@
 import 'package:codekaze_free_ui_kit/core.dart';
 import 'package:flutter/material.dart';
+import 'package:codekaze_free_ui_kit/module/course_calendar/controller/course_calendar_controller.dart';
+
+import 'package:intl/intl.dart';
 
 import 'package:get/get.dart';
 
@@ -16,53 +19,124 @@ class CourseCalendarView extends StatelessWidget {
           appBar: AppBar(
             title: Text("Course Calendar"),
           ),
-          body: Column(
+          body: Stack(
             children: [
               Container(
-                height: 140.0,
-                color: Colors.green,
-              ),
-              Expanded(
-                child: SelectList(
-                  items: [
-                    {
-                      "course_name": "Matemathics",
-                      "sub_title": "Chapter 1: Introduction",
-                      "room_name": "Room 6-205",
-                      "start_time": "11:35",
-                      "end_time": "13:05",
-                      "author": {
-                        "name": "James Barley",
-                        "avatar_url":
-                            "https://images.pexels.com/photos/1043471/pexels-photo-1043471.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-                      },
-                    },
-                    {
-                      "course_name": "Matemathics",
-                      "sub_title": "Chapter 1: Introduction",
-                      "room_name": "Room 6-205",
-                      "start_time": "11:35",
-                      "end_time": "13:05",
-                      "author": {
-                        "name": "James Barley",
-                        "avatar_url":
-                            "https://images.pexels.com/photos/1043471/pexels-photo-1043471.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-                      },
-                    },
-                    {
-                      "course_name": "Matemathics",
-                      "sub_title": "Chapter 1: Introduction",
-                      "room_name": "Room 6-205",
-                      "start_time": "11:35",
-                      "end_time": "13:05",
-                      "author": {
-                        "name": "James Barley",
-                        "avatar_url":
-                            "https://images.pexels.com/photos/1043471/pexels-photo-1043471.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-                      },
-                    }
-                  ],
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 10, left: 20, right: 20),
+                  child: Container(
+                    height: 80,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          child: Row(
+                            children: [
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    DateFormat.d().format(DateTime.now()),
+                                    style: TextStyle(
+                                      fontSize: 40,
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    DateFormat.EEEE().format(DateTime.now()),
+                                    style: TextStyle(
+                                      color: Colors.grey[400],
+                                    ),
+                                  ),
+                                  Text(
+                                    (DateFormat.yMMM().format(DateTime.now())),
+                                    style: TextStyle(
+                                      color: Colors.grey[400],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                height: 40,
+                                width: 80,
+                                decoration: BoxDecoration(
+                                  color: Colors.green[50],
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                child: Center(
+                                  child: Text(
+                                    "Today",
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                      color: Colors.green,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
+              ),
+              SelectList(
+                items: [
+                  {
+                    "course_name": "English",
+                    "sub_title": "Part 5 : Causative Verbs",
+                    "room_name": "Room A - 201",
+                    "start_time": "10.00",
+                    "end_time": "11:30",
+                    "author": {
+                      "name": "Bevelyn C.",
+                      "avatar_url":
+                          "https://images.unsplash.com/photo-1629316464107-ec2702d7e10c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
+                    },
+                  },
+                  {
+                    "course_name": "Science",
+                    "sub_title": "Part 6 : Human Genetics",
+                    "room_name": "Room B - 202",
+                    "start_time": "13:30",
+                    "end_time": "15:00",
+                    "author": {
+                      "name": "George Leonard",
+                      "avatar_url":
+                          "https://images.pexels.com/photos/1043471/pexels-photo-1043471.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+                    },
+                  },
+                  {
+                    "course_name": "Social Studies",
+                    "sub_title": "Part 4 : World History",
+                    "room_name": "Room D - 301",
+                    "start_time": "15:30",
+                    "end_time": "16:30",
+                    "author": {
+                      "name": "Johan Bridge",
+                      "avatar_url":
+                          "https://images.unsplash.com/photo-1629473728190-c9a984fed794?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=332&q=80",
+                    },
+                  }
+                ],
               ),
             ],
           ),
--- lib/module/home/view/select_list.dart
+++ lib/module/home/view/select_list.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:date_picker_timeline/date_picker_timeline.dart';
+
 class SelectList extends StatefulWidget {
   final List items;
   SelectList({
@@ -13,60 +15,279 @@ class SelectList extends StatefulWidget {
 class _SelectListState extends State<SelectList> {
   int selectedIndex = -1;
 
+  final DatePickerController _controller = DatePickerController();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: ListView.builder(
-        itemCount: widget.items.length,
-        itemBuilder: (context, index) {
-          var item = widget.items[index];
-
-          bool isSelected = selectedIndex == index;
-
-          return InkWell(
-            onTap: () {
-              selectedIndex = index;
-              setState(() {});
-            },
-            child: Container(
-              height: 140.0,
-              margin: EdgeInsets.only(bottom: 10.0),
-              padding: EdgeInsets.all(12.0),
-              child: Row(
-                children: [
+    return Padding(
+      padding: const EdgeInsets.only(top: 100),
+      child: Container(
+        height: 500,
+        width: 500,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
                   Container(
-                    width: 50.0,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(item["start_time"]),
-                        Text(item["end_time"]),
-                      ],
-                    ),
-                  ),
-                  Expanded(
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: isSelected ? Colors.red : Colors.blue,
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(20.0),
-                        ),
-                      ),
-                      child: Column(
-                        children: [
-                          Text(item["course_name"]),
-                          Text(item["sub_title"]),
-                          Text(item["room_name"]),
-                          Text(item["author"]["name"]),
-                        ],
-                      ),
+                    child: DatePicker(
+                      DateTime.now(),
+                      width: 60,
+                      height: 90,
+                      controller: _controller,
+                      initialSelectedDate: DateTime.now(),
+                      selectionColor: Colors.red,
+                      selectedTextColor: Colors.white,
                     ),
                   ),
                 ],
               ),
             ),
-          );
-        },
+            Divider(
+              height: 0,
+            ),
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.only(
+                  left: 20,
+                  right: 20,
+                ),
+                child: Column(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        child: ListView.builder(
+                            itemCount: widget.items.length,
+                            itemBuilder: (context, index) {
+                              var item = widget.items[index];
+                              bool isSelected = selectedIndex == index;
+
+                              return InkWell(
+                                onTap: () {
+                                  selectedIndex = index;
+                                  setState(() {});
+                                },
+                                child: Column(
+                                  children: [
+                                    if (index == 0)
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: [
+                                          Container(
+                                            child: Row(
+                                              children: [
+                                                Text(
+                                                  "Time",
+                                                  style: TextStyle(
+                                                    color: Colors.grey,
+                                                  ),
+                                                ),
+                                                SizedBox(
+                                                  width: 35,
+                                                ),
+                                                Text(
+                                                  "Course",
+                                                  style: TextStyle(
+                                                    color: Colors.grey,
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                                          ),
+                                          InkWell(
+                                            onTap: () {},
+                                            child: Icon(
+                                              Icons.format_line_spacing,
+                                              color: Colors.grey[400],
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    if (index == 0)
+                                      SizedBox(
+                                        height: 15,
+                                      ),
+                                    IntrinsicHeight(
+                                      child: Row(
+                                        children: [
+                                          Column(
+                                            children: [
+                                              SizedBox(
+                                                height: 10,
+                                              ),
+                                              Text(
+                                                item["start_time"],
+                                                style: TextStyle(
+                                                  fontSize: 15,
+                                                  fontWeight: FontWeight.bold,
+                                                ),
+                                              ),
+                                              Text(
+                                                item["end_time"],
+                                                style: TextStyle(
+                                                  color: Colors.grey[400],
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                          VerticalDivider(
+                                            width: 30,
+                                          ),
+                                          Expanded(
+                                            child: Container(
+                                              padding: EdgeInsets.only(
+                                                top: 5,
+                                                bottom: 5,
+                                              ),
+                                              child: Container(
+                                                padding: EdgeInsets.only(
+                                                  left: 15,
+                                                  top: 20,
+                                                  right: 15,
+                                                  bottom: 20,
+                                                ),
+                                                width: 250,
+                                                decoration: BoxDecoration(
+                                                  color: isSelected
+                                                      ? Colors.green
+                                                      : Colors.grey[100],
+                                                  borderRadius:
+                                                      BorderRadius.circular(15),
+                                                ),
+                                                child: Column(
+                                                  children: [
+                                                    Row(
+                                                      mainAxisAlignment:
+                                                          MainAxisAlignment
+                                                              .spaceBetween,
+                                                      children: [
+                                                        Text(
+                                                          item["course_name"],
+                                                          style: TextStyle(
+                                                            color: isSelected
+                                                                ? Colors.white
+                                                                : Colors.black,
+                                                            fontSize: 16,
+                                                            fontWeight:
+                                                                FontWeight.bold,
+                                                          ),
+                                                        ),
+                                                        InkWell(
+                                                          onTap: () {},
+                                                          child: Icon(
+                                                            Icons.more_vert,
+                                                            size: 20,
+                                                            color: isSelected
+                                                                ? Colors.white
+                                                                : Colors.black,
+                                                          ),
+                                                        ),
+                                                      ],
+                                                    ),
+                                                    SizedBox(
+                                                      height: 5,
+                                                    ),
+                                                    Row(
+                                                      children: [
+                                                        Text(
+                                                          item["sub_title"],
+                                                          style: TextStyle(
+                                                            color: isSelected
+                                                                ? Colors.white
+                                                                : Colors.black,
+                                                            fontSize: 13,
+                                                            fontWeight:
+                                                                FontWeight.bold,
+                                                          ),
+                                                        ),
+                                                      ],
+                                                    ),
+                                                    SizedBox(
+                                                      height: 10,
+                                                    ),
+                                                    Row(
+                                                      children: [
+                                                        Icon(
+                                                          Icons
+                                                              .location_on_outlined,
+                                                          size: 16,
+                                                          color: isSelected
+                                                              ? Colors.white
+                                                              : Colors.black,
+                                                        ),
+                                                        SizedBox(
+                                                          width: 5,
+                                                        ),
+                                                        Text(
+                                                          item["room_name"],
+                                                          style: TextStyle(
+                                                            color: isSelected
+                                                                ? Colors.white
+                                                                : Colors.black,
+                                                            fontSize: 13,
+                                                          ),
+                                                        ),
+                                                      ],
+                                                    ),
+                                                    SizedBox(
+                                                      height: 10,
+                                                    ),
+                                                    Row(
+                                                      children: [
+                                                        CircleAvatar(
+                                                          radius: 8,
+                                                          backgroundImage:
+                                                              NetworkImage(
+                                                            item["author"]
+                                                                ["avatar_url"],
+                                                          ),
+                                                        ),
+                                                        SizedBox(
+                                                          width: 5,
+                                                        ),
+                                                        Text(
+                                                          item["author"]
+                                                              ["name"],
+                                                          style: TextStyle(
+                                                            color: isSelected
+                                                                ? Colors.white
+                                                                : Colors.black,
+                                                            fontSize: 13,
+                                                          ),
+                                                        ),
+                                                      ],
+                                                    ),
+                                                  ],
+                                                ),
+                                              ),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                    if (index == 2)
+                                      SizedBox(
+                                        height: 30,
+                                      ),
+                                  ],
+                                ),
+                              );
+                            }),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
